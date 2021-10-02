8bbf7f5e45a586b8d18b469670e72a5290724158
Pavan <pavanjanapati1216@gmail.com>
2021-10-02 14:32:41 +0530

    EEE

--- GitToolApp/src/main/java/com/vrnda/test/Test.java
+++ GitToolApp/src/main/java/com/vrnda/test/Test.java
@@ -3,7 +3,7 @@ package com.vrnda.test;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("I am Test.main() Method");
+		System.out.println("I am Test.main()");
 
 	}
 

4f107995bb2c16f0ef7290fda0820986f16c0713
Pavan <pavanjanapati1216@gmail.com>
2021-10-02 14:54:32 +0530

    SSs

--- GitToolApp/src/main/java/com/vrnda/test/Test.java
+++ GitToolApp/src/main/java/com/vrnda/test/Test.java
@@ -3,7 +3,7 @@ package com.vrnda.test;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("I am Test.main()");
+		System.out.println("I am Test.main( )");
 
 	}
 
